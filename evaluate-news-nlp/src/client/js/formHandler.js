c6ee2faa7f4f8a0e5e20621579661e1109e34d62
Boyu1997 <jiangby97@gmail.com>
2021-01-27 15:58:53 -0500

    webpack and server setup

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,11 +3,14 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
+    const txt = document.querySelector('#name').value
+    
+    fetch(`http://localhost:8081/data?txt=${txt}`)
+    .then(res => {
+        console.log(res)
+        res.json()})
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
     })
--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,51 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fetch = require("node-fetch")
+
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1"
+
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+app.get('/data', async function (req, res) {
+    const txt = req.query.txt
+    console.log(txt)
+
+    const response = await fetch(
+        `${baseUrl}?key=${process.env.API_KEY}&of=json&txt=${txt}&lang=en`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+    );
+
+    try {
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.log("error", error);
+    }
 })
