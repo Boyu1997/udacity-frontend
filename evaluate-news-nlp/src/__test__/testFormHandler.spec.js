0279ef3eb915efa196d521b4fb1a39e3a23d85e2
Boyu1997 <jiangby97@gmail.com>
2021-01-28 18:31:03 -0500

    Fix testing

--- evaluate-news-nlp/src/__test__/testFormHandler.spec.js
+++ evaluate-news-nlp/src/__test__/testFormHandler.spec.js
@@ -2,6 +2,8 @@ import { handleSubmit } from '../client/js/formHandler'
 import { validateText } from '../client/js/validateText'
 
 describe("Testing the submit functionality", () => {
+    window.alert = jest.fn();
+    
     test("Testing the handleSubmit() function", () => {
         expect(handleSubmit).toBeDefined()
     })
@@ -9,5 +11,6 @@ describe("Testing the submit functionality", () => {
         expect(validateText).toBeDefined();
         expect(validateText("This, should be A? valid input..&")).toBe(true)
         expect(validateText("µ©¿é are not")).toBe(false)
+        window.alert.mockClear(); 
     })
 })
